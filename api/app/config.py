b5874532b5aa4174d3b7da3f00148771a2aa12ed
Toni Miquel <tonimiquel.llull@gmail.com>
2023-09-29 20:23:34 +0200

    WIP: fastapi

---
+++ api/app/config.py
@@ -0,0 +1,54 @@
+import json
+import logging
+import os
+
+from dotenv import dotenv_values
+
+
+class Config:
+    def __init__(self):
+        try:
+            self.ALLOWED_USERS = json.loads(os.environ["ALLOWED_USERS"])
+            self.ALLOWED_USERS_RESET = json.loads(os.environ["ALLOWED_USERS_RESET"])
+            self.TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+            self.ADMIN_USERS = json.loads(os.environ["ADMIN_USERS"])
+            self.TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+            self.TELEGRAM_ADMIN_CHAT_ID = os.environ["TELEGRAM_ADMIN_CHAT_ID"]
+            self.GOOGLE_DRIVE_URL = os.environ["GOOGLE_DRIVE_URL"]
+            self.DB_HOST = os.environ["DB_HOST"]
+            self.DB_NAME = os.environ["DB_NAME"]
+            self.DB_USER = os.environ["DB_USER"]
+            self.DB_PASS = os.environ["DB_PASS"]
+            self.CLOCKIFY_BASEURL = os.environ["CLOCKIFY_BASEURL"]
+            self.CLOCKIFY_USERS = json.loads(os.environ["CLOCKIFY_USERS"])
+            self.CLOCKIFY_USERS_API = json.loads(os.environ["CLOCKIFY_USERS_API"])
+            self.CLOCKIFY_WORKSPACE = os.environ["CLOCKIFY_WORKSPACE"]
+            self.DB_MODE = os.environ["DB_MODE"]
+            self.N8N_BASE_URL = os.environ["N8N_BASE_URL"]
+            self.N8N_WH_ADD_GAME = os.environ["N8N_WH_ADD_GAME"]
+            self.HEALTHCHECKS = os.environ["HEALTHCHECKS"]
+            self.CLOCKIFY_ADMIN_API_KEY = os.environ["CLOCKIFY_ADMIN_API_KEY"]
+
+        except Exception:
+            # Load .env
+            config = dotenv_values(".env")
+            self.ALLOWED_USERS = json.loads(config["ALLOWED_USERS"])
+            self.ALLOWED_USERS_RESET = json.loads(config["ALLOWED_USERS_RESET"])
+            self.ADMIN_USERS = json.loads(config["ADMIN_USERS"])
+            self.TELEGRAM_TOKEN = config["TELEGRAM_TOKEN"]
+            self.GOOGLE_DRIVE_URL = config["GOOGLE_DRIVE_URL"]
+            self.TELEGRAM_CHAT_ID = config["TELEGRAM_CHAT_ID"]
+            self.TELEGRAM_ADMIN_CHAT_ID = config["TELEGRAM_ADMIN_CHAT_ID"]
+            self.DB_HOST = config["DB_HOST"]
+            self.DB_NAME = config["DB_NAME"]
+            self.DB_USER = config["DB_USER"]
+            self.DB_PASS = config["DB_PASS"]
+            self.CLOCKIFY_BASEURL = config["CLOCKIFY_BASEURL"]
+            self.CLOCKIFY_USERS = json.loads(config["CLOCKIFY_USERS"])
+            self.CLOCKIFY_USERS_API = json.loads(config["CLOCKIFY_USERS_API"])
+            self.CLOCKIFY_WORKSPACE = config["CLOCKIFY_WORKSPACE"]
+            self.DB_MODE = config["DB_MODE"]
+            self.N8N_BASE_URL = config["N8N_BASE_URL"]
+            self.N8N_WH_ADD_GAME = config["N8N_WH_ADD_GAME"]
+            self.HEALTHCHECKS = config["HEALTHCHECKS"]
+            self.CLOCKIFY_ADMIN_API_KEY = config["CLOCKIFY_ADMIN_API_KEY"]
--- api/app/database.py
+++ api/app/database.py
@@ -2,11 +2,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from .config import Config
+
+config = Config()
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    f"mysql+pymysql://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}/{config.DB_NAME}",
+    connect_args={
+        "ssl": {
+            "ca": None,
+        }
+    },
+    pool_size=20,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

953b8e321ba48d5fd7b063ebf9c5f3a67eea5dc7
Toni Miquel <tonimiquel.llull@gmail.com>
2024-01-06 11:32:53 +0100

    Move 'playing' endpoint from users to utils

--- api/app/routers/utils.py
+++ api/app/routers/utils.py
@@ -3,6 +3,7 @@ from fastapi_versioning import version
 from sqlalchemy.orm import Session
 
 from .. import auth
+from ..crud import games, time_entries, users
 from ..crud.achievements import Achievements
 from ..database import models, schemas
 from ..database.database import SessionLocal, engine
@@ -55,3 +56,25 @@ def achievements_list(
     for ach in ach_list:
         response.append(ach.title)
     return response
+
+
+@router.get("/playing")
+@version(1)
+def get_playing_users(db: Session = Depends(get_db)):
+    """
+    Get all users
+    """
+    users_db = users.get_users(db)
+    playing = []
+    for user in users_db:
+        info = {}
+        active_timer = time_entries.get_active_time_entry_by_user(db, user)
+        if active_timer is not None:
+            logger.info(active_timer)
+            info["user"] = user.name
+            info["game"] = games.get_game_by_id(
+                db, active_timer.project_clockify_id
+            ).name
+            info["time"] = active_timer.start
+            playing.append(info)
+    return playing

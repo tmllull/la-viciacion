0a51f107313c4062a29263c5120f72f173b20367
Toni Miquel <tonimiquel.llull@gmail.com>
2024-08-21 07:11:22 +0100

    Remove basicConfig loggins to avoid recurrent httpsx logs from Telegram Bot

--- bot/app/utils/logger.py
+++ bot/app/utils/logger.py
@@ -1,9 +1,9 @@
 import logging
 import sys
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+# logging.basicConfig(
+#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+# )
 logger = logging.getLogger(__name__)
 if not logger.handlers:
     logger.propagate = False

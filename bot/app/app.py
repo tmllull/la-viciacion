13252c30ebe6d7f8751a281ae6e3ba2af2d28589
Toni Miquel <tonimiquel.llull@gmail.com>
2024-09-01 09:23:30 +0100

    Remove logs for Sentry

--- bot/app/app.py
+++ bot/app/app.py
@@ -55,7 +55,7 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
     environment=config.ENVIRONMENT,
-    before_send=before_send,
+    # before_send=before_send,
 )
 
 FILTER_YES = "^(✅ Sí)$"
